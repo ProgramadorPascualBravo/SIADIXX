5324633c26dac3ec848c88dc269f4c03f92b32cb
Juan Duque <juuanduuke@gmail.com>
2021-05-03 11:50:05 -0500

    fixed bug alerts and field model User

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'last_name', 'username', 'password', 'department_id', 'state', 'verified'
+        'name', 'last_name', 'username', 'password', 'department_id', 'state', 'verified', 'document'
     ];
 
     /**
