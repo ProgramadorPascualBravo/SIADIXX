3e4c307acb54655495ea2fc99b0854a2968afcf9
Juan Duque <juuanduuke@gmail.com>
2021-06-30 15:57:39 -0500

    Img description of permission

--- resources/views/livewire/permission-role/permission-component.blade.php
+++ resources/views/livewire/permission-role/permission-component.blade.php
@@ -16,6 +16,14 @@
                     @enderror
                 </label>
         </div>
+        <div class="col-span-2">
+            <a href="{{ asset('img/permisos.JPG') }}" target="_blank" class="rounded-lg shadow-lg p-5 text-sm uppercase border-gray-300 border cursor-pointer">
+                Descripción de los permisos
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-siadi-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </div>
         @if (Auth::user()->can('permission_write'))
         <div class="col-span-4 pl-4 mt-1 flex flex-row-reverse">
             <button class="btn btn-red inline-flex" type="submit" wire:click="cancel">Cancelar</button>
